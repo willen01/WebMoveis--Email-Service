32bb46ce96c7c9a9ec9f8fe81b989fb1bb20a721
willen <willen.sesantos@gmail.com>
2022-11-22 21:30:33 -0300

    fix: add PORT

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
 
   //inicializando microsserviços
   await app.startAllMicroservices();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
